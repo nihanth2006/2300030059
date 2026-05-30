8b7fdb64a581b55ea5ddd9f399d27f9a4301ac33
B.NIHANTH <nihanth1006@gmail.com>
2026-05-30 13:22:31 +0530

    Update NotificationCard.jsx

--- src/components/NotificationCard.jsx
+++ src/components/NotificationCard.jsx
@@ -1,9 +1,23 @@
+import { Log } from "../utils/logger";
+
 function NotificationCard({ notification, rank, viewed, onToggleViewed }) {
+  const handleToggleViewed = async () => {
+    await Log(
+      "info",
+      "component",
+      `${notification.Type} notification marked as ${
+        viewed ? "unread" : "read"
+      }`
+    );
+
+    onToggleViewed();
+  };
+
   return (
     <div className={`notification-card ${viewed ? "viewed" : "unread"}`}>
       <div className="notification-card-header">
         <div className="rank">#{rank}</div>
-        <button className="view-toggle" onClick={onToggleViewed}>
+        <button className="view-toggle" onClick={handleToggleViewed}>
           {viewed ? "Mark unread" : "Mark read"}
         </button>
       </div>
